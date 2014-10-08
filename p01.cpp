42ba6fbcc6f2b7bcfa7439ef1f17697bc84aac57
Steven <steven.chambers@cox.net>
2014-10-08 16:02:35 -0500

    make output file more readable when verifying program

--- p01.cpp
+++ p01.cpp
@@ -131,69 +131,69 @@ int main(int argc, char* argv[])
 			{
 				case '+':	
 						o << setw(20) << setfill('-')<< "-" << endl;
-						o << "Initial conditions: R0: " << R0.printReg() << " R1: " << R1.printReg() << endl;	
+						o << "Initial conditions:" << endl << "R0: " << R0.printReg() <<endl << "R1: " << R1.printReg() << endl;	
 						o << "op code: 000 operation: R0 <-- R0 XOR R1"<<endl;
 						R0.regLoad(R0.regXOR(R1));
-						o << "Post operation conditions: R0: " << R0.printReg() << " R1: "<< R1.printReg() << endl;
+						o << "Post operation conditions:" << endl << "R0: " << R0.printReg() <<endl <<  "R1: "<< R1.printReg() << endl;
 						o << setw(20) << setfill('-')<< "-" << endl << endl;	
 						break;	
 				case 'v':
 						
 						o << setw(20) << setfill('-')<< "-" << endl;
-						o << "Initial conditions: R0: " << R0.printReg() << " R1: " << R1.printReg() << endl;	
+						o << "Initial conditions:" << endl << "R0: " << R0.printReg() <<endl << "R1: " << R1.printReg() << endl;	
 						o << "op code: 001 operation: R0 <-- R0 OR R1"<<endl;
 						R0.regLoad(R0.regOr(R1));
-						o << "Post operation conditions: R0: " << R0.printReg() << " R1: "<< R1.printReg() << endl;
+						o << "Post operation conditions:" << endl << "R0: " << R0.printReg() <<endl <<  "R1: "<< R1.printReg() << endl;
 						o << setw(20) << setfill('-')<< "-" << endl << endl;	
 						break;
 				case '-':
 						o << setw(20) << setfill('-')<< "-" << endl;	
-						o << "Initial conditions: R0: " << R0.printReg() << " R1: " << R1.printReg() << endl;	
+						o << "Initial conditions:" << endl << "R0: " << R0.printReg() <<endl << "R1: " << R1.printReg() << endl;	
 						o << "op code: 010 operation: R0 <-- R0 XNOR R1"<<endl; 
 						R0.regLoad(R0.regXNOR(R1));	
-						o << "Post operation conditions: R0: " << R0.printReg() << " R1: "<< R1.printReg() << endl;
+						o << "Post operation conditions:" << endl << "R0: " << R0.printReg() <<endl <<  "R1: "<< R1.printReg() << endl;
 						o << setw(20) << setfill('-')<< "-" << endl << endl;	
 						break;
 				case '^':
 						o << setw(20) << setfill('-')<< "-" << endl;	
-						o << "Initial conditions: R0: " << R0.printReg() << " R1: " << R1.printReg() << endl;	
+						o << "Initial conditions:" << endl << "R0: " << R0.printReg() <<endl << "R1: " << R1.printReg() << endl;	
 						o << "op code: 011 operation: R0 <-- R0 AND R1"<<endl;
 						R0.regLoad(R0.regAnd(R1));
-						o << "Post operation conditions: R0: " << R0.printReg() << " R1: "<< R1.printReg() << endl;
+						o << "Post operation conditions:" << endl << "R0: " << R0.printReg() <<endl <<  "R1: "<< R1.printReg() << endl;
 						o << setw(20) << setfill('-')<< "-" << endl << endl;	
 						break;
 				case 'l':
 						o << setw(20) << setfill('-')<< "-" << endl;	
-						o << "Initial conditions: R0: " << R0.printReg() << " R1: " << R1.printReg() << endl;	
+						o << "Initial conditions:" << endl << "R0: " << R0.printReg() <<endl << "R1: " << R1.printReg() << endl;	
 						o << "op code: 100 operation: R0 <-- D"<<endl;
 						R0.regLoad(dataString);
-						o << "Post operation conditions: R0: " << R0.printReg() << " R1: "<< R1.printReg() << endl;
+						o << "Post operation conditions:" << endl << "R0: " << R0.printReg() <<endl <<  "R1: "<< R1.printReg() << endl;
 						o << setw(20) << setfill('-')<< "-" << endl<< endl;
 						break;
 				case 'L':
 						o << setw(20) << setfill('-')<< "-" << endl;	
-						o << "Initial conditions: R0: " << R0.printReg() << " R1: " << R1.printReg() << endl;	
+						o << "Initial conditions:" << endl << "R0: " << R0.printReg() <<endl << "R1: " << R1.printReg() << endl;	
 						o << "op code: 101 operation: R1 <-- D"<<endl;
 						R1.regLoad(dataString);
-						o << "Post operation conditions: R0: " << R0.printReg() << " R1: "<< R1.printReg() << endl;
+						o << "Post operation conditions:" << endl << "R0: " << R0.printReg() <<endl <<  "R1: "<< R1.printReg() << endl;
 						o << setw(20) << setfill('-')<< "-" << endl<< endl;	
 						break;
 				case 'p':
 						o << setw(20) << setfill('-')<< "-" << endl;	
-						o << "Initial conditions: R0: " << R0.printReg() << " R1: " << R1.printReg() << endl;	
+						o << "Initial conditions:" << endl << "R0: " << R0.printReg() <<endl << "R1: " << R1.printReg() << endl;	
 						o << "op code: 110 operation: Print R0 "<<endl;
 						o <<"R0 : " << R0.printReg() << endl;
 						cout <<"R0: " <<  R0.printReg() << endl;
-						o << "Post operation conditions: R0: " << R0.printReg() << " R1: "<< R1.printReg() << endl;
+						o << "Post operation conditions:" << endl << "R0: " << R0.printReg() <<endl <<  "R1: "<< R1.printReg() << endl;
 						o << setw(20) << setfill('-')<< "-" << endl<< endl;	
 						break;
 				case 'P':
 						o << setw(20) << setfill('-')<< "-" << endl;	
-						o << "Initial conditions: R0: " << R0.printReg() << " R1: " << R1.printReg() << endl;	
+						o << "Initial conditions:" << endl << "R0: " << R0.printReg() <<endl << "R1: " << R1.printReg() << endl;	
 						o << "op code: 111 operation: Print R1"<<endl;
 						o << "R1: " <<R1.printReg() << endl;
 						cout <<"R1: " << R1.printReg() << endl;
-						o << "Post operation conditions: R0: " << R0.printReg() << " R1: "<< R1.printReg() << endl;
+						o << "Post operation conditions:" << endl << "R0: " << R0.printReg() <<endl <<  "R1: "<< R1.printReg() << endl;
 						o << setw(20) << setfill('-')<< "-" << endl<< endl;	
 						break;
 				default:
